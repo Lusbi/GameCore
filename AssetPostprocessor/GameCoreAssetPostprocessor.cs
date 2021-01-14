eabf8d4bf1c1d9da7463770cb5490e3c4ae34603
cutelusbi <cutelusbi@gmail.com>
2021-01-14 18:34:19 +0800

    Base Postprocessor

---
+++ AssetPostprocessor/GameCoreAssetPostprocessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameCoreAssetPostprocessor
+{
+    public abstract string assetPostprocessorName { get; }
+    public bool isWork { get; set; }
+    
+}
